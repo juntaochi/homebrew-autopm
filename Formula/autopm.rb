b9486440d7b3d06240db91de968f543032c65d5a
Juntao Chi <476409177@qq.com>
2026-07-10 19:59:23 +0800

    Update autopm 0.0.3 artifact

--- Formula/autopm.rb
+++ Formula/autopm.rb
@@ -4,7 +4,7 @@ class Autopm < Formula
   desc "Standalone AutoPM agent with native product-management commands and skills"
   homepage "https://github.com/juntaochi/autopm"
   url "https://registry.npmjs.org/@jacfrost/autopm/-/autopm-0.0.3.tgz"
-  sha256 "70fd4baeb807f69625392a029444ad2170935918eb912b6f62f9f5588d28d5d3"
+  sha256 "6442a65f30c79cc676dd39a5dfdbdb9182947d15b6832d71390bf016eb6b44be"
   license :cannot_represent # package.json declares UNLICENSED
 
   depends_on "node"
